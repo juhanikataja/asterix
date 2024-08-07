46ed00fe70ed0160b077bdbd11e2cc20b6a4f429
kstppd <kpapadakis@protonmail.com>
2024-08-07 21:41:24 +0300

    Add tensorboard to vqvae training

--- src/assessment/vqvae/vqvae_train.py
+++ src/assessment/vqvae/vqvae_train.py
@@ -12,12 +12,65 @@ from torchvision import datasets, transforms
 from torch.multiprocessing import Process
 import torch.multiprocessing as mp
 from torch.multiprocessing import Process
+from torch.utils.tensorboard import SummaryWriter
 
+use_tensorboard=False
+writer = SummaryWriter(log_dir="./log")
+
+
+def plot_vdfs_tb(a, b, vdf_vmin=1e-16):
+    from matplotlib import rcParams
+    from scipy import ndimage
+    import shutil
+    rcParams['text.usetex']= True if shutil.which('latex') else False
+    import matplotlib.pyplot as plt
+    from matplotlib.colors import LogNorm
+    nx, ny, nz = np.shape(a)
+    fig, ax = plt.subplots(2, 3)
+
+
+    slicer2d = np.s_[:, :, nz // 2]
+    slicer1d = np.s_[:, ny // 2, nz // 2]
+    im1 = ax[0, 0].imshow(a[slicer2d])
+    im2 = ax[0, 1].imshow(b[slicer2d])
+    ax[1, 0].semilogy(b[slicer1d], label="Reconstructed")
+    ax[1, 0].semilogy(a[slicer1d], label="Original")
+    ax2 = ax[1, 0].twinx()
+    ax2.plot(b[slicer1d] - a[slicer1d], label="Difference recon-orig", color="k")
+    yabs_max = abs(max(ax2.get_ylim(), key=abs))
+    ax2.set_ylim(ymin=-yabs_max, ymax=yabs_max)
+    im4 = ax[1, 1].imshow(np.abs(a[slicer2d] - b[slicer2d]))
+    plt.colorbar(im1)
+    plt.colorbar(im2)
+    plt.colorbar(im4)
+    ax[0, 0].set_title("Original VDF")
+    ax[0, 1].set_title("Reconstructed VDF")
+    ax[1, 0].set_title("Profile")
+    ax[1, 1].set_title("Absolute Diff")
+    ax[1, 0].legend()
+    ax2.legend()
+
+    grad_a = np.stack(np.gradient(a), axis=-1)
+    grad_b = np.stack(np.gradient(b), axis=-1)
+    diff_grads = np.linalg.norm(grad_a - grad_b, axis=-1)
+
+    im5 = ax[0, 2].imshow(diff_grads[slicer2d], cmap="batlow")
+    ax[0, 2].set_title("norm(diff of gradient vectors)")
+    plt.colorbar(im5)
+
+    lapl_a = ndimage.gaussian_laplace(a, 1)
+    lapl_b = ndimage.gaussian_laplace(b, 1)
+
+    diff_lapls = lapl_a - lapl_b
+
+    im6 = ax[1, 2].imshow(diff_grads[slicer2d], cmap="seismic")
+    ax[1, 2].set_title("diff of (gaussian[1]) laplacians")
+    plt.colorbar(im6)
+    return fig
 
 
 def main():
     mp.set_start_method('spawn')
-
 
     dist.init_process_group(backend='nccl')
     rank = int(os.environ['LOCAL_RANK'])
@@ -45,7 +98,7 @@ def main():
     model = VQVAE(**model_args).to(device)
     model = DDP(model)
 
-    batch_size =128
+    batch_size =2
     epochs = 100
     workers = 4
     f = pt.vlsvfile.VlsvReader(filename)
@@ -80,6 +133,7 @@ def main():
 
 
 
+    counter=0
     for epoch in range(epochs):
         train_sampler.set_epoch(epoch)
         total_train_loss = 0
@@ -87,7 +141,7 @@ def main():
         n_train = 0
         for batch_idx, train_tensors in enumerate(train_loader):
             optimizer.zero_grad()
-            imgs = train_tensors[0].unsqueeze(0).to(rank)
+            imgs = train_tensors.to(rank)
             out = model(imgs)
             recon_error = criterion(out["x_recon"], imgs)
             total_recon_error += recon_error.item()
@@ -99,6 +153,13 @@ def main():
             loss.backward()
             optimizer.step()
             n_train += 1
+            
+            if global_rank and use_tensorboard==0:
+                temp=imgs.cpu().detach().numpy()
+                reconstructions = out["x_recon"].cpu().detach().numpy()
+                fig=plot_vdfs_tb(temp[0,0,:,:,:], reconstructions[0,0,:,:,:] , vdf_vmin=1e-16)
+                writer.add_figure('State',fig,counter)
+                counter+=1
 
             if ((batch_idx + 1) % eval_every) == 0 and global_rank == 0:
                 print(f"epoch: {epoch}\nbatch_idx: {batch_idx + 1}", flush=True)
